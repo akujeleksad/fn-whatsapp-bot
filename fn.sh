8d5efc853bdcbfe70a7ed0cd28b5ab8417d2b95e
FN <70086013+Terror-Machine@users.noreply.github.com>
2020-12-06 18:51:39 +0700

    oke

--- fn.sh
+++ fn.sh
@@ -18,6 +18,7 @@ sudo apt-get install -y gifsicle
 sudo apt-get install -y tesseract-ocr
 sudo apt-get install -y libtesseract-dev
 sudo apt-get install -y curl openssl libssl-dev
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev libgif-dev
 sudo apt-get install -y libicu-dev libpango1.0-dev libcairo2-dev
 sudo apt-get update && sudo apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 sudo timedatectl set-timezone Asia/Jakarta
